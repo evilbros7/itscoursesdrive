67b15e9db178d5c263697165888dc8d7ce2911d5
S M Mahabub Hossain <77372461+iamSMMH@users.noreply.github.com>
2021-12-06 09:26:42 -0700

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -41,7 +41,6 @@ class _BotCommands:
         self.QbZipMirrorCommand = getCommand('QBZIPMIRROR_COMMAND', f'qbzipmirror{BOT_NO}')
         self.DeleteCommand = getCommand('DELETE_COMMAND', f'del{BOT_NO}')
         self.ConfigMenuCommand = getCommand('CONFIGMENU_COMMAND', f'config{BOT_NO}')
-        self.MediaInfoCommand = getCommand('MEDIAINFO_COMMAND', f'mediainfo{BOT_NO}')
         self.UsageCommand = getCommand('USAGE_COMMAND', f'usage{BOT_NO}')
         self.ShellCommand = getCommand('SHELL_COMMAND', f'shell{BOT_NO}')
         self.TsHelpCommand = getCommand('TSHELP_COMMAND', f'tshelp{BOT_NO}')
