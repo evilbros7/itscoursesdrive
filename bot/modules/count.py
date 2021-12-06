82a8ef196127d5746208d83391c9dfe43069e3da
S M Mahabub Hossain <77372461+iamSMMH@users.noreply.github.com>
2021-12-06 09:54:24 -0700

    Update count.py

--- bot/modules/count.py
+++ bot/modules/count.py
@@ -2,7 +2,7 @@ from telegram.ext import CommandHandler
 
 from bot import dispatcher
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
-from bot.helper.telegram_helper.message_utils import deleteMessage, sendMessage
+from bot.helper.telegram_helper.message_utils import *
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.ext_utils.bot_utils import is_gdrive_link, is_gdtot_link
